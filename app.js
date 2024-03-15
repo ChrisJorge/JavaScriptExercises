c5ca2560d94029c66cb7e7c21fb763f4cdfd2edd
Chris <Christopherjorge47@icloud.com>
2024-03-15 09:19:27 -0400

    Completed first question

---
+++ app.js
@@ -0,0 +1,16 @@
+// Count down to 0 from a given number.
+let n = 20
+for (n; n > 0; n-- ){
+    console.log(n)
+}
+// Log integers in multiples of 3 as long as they are less than 35.
+
+// Print integers in multiples of 5 as long as they are less than 100.
+
+// Print integers between 0 and 20 with the following conditions:
+
+// All numbers divisible by 2 should be multiplied by 3 before they are output.
+
+// All other integers should not be output.
+
+// Print all prime numbers between 0 and 20.
